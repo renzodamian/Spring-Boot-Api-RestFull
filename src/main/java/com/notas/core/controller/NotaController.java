9a9bcb2981514394faf46110779b38d8b9ef50f9
renzodamian <renzojaramillocueva@gmail.com>
2019-04-10 21:49:56 -0500

    Controller Delete

--- src/main/java/com/notas/core/controller/NotaController.java
+++ src/main/java/com/notas/core/controller/NotaController.java
@@ -4,6 +4,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +32,12 @@ public class NotaController {
 	@PostMapping("/nota")
 	public boolean actualizarNota(@RequestBody @Valid Nota nota) {
 		return service.actualizar(nota);
+	}
+	
+	@DeleteMapping("/nota/{id}/{nombre}")
+	public boolean borrarNota(@PathVariable("id")long id, 
+			@PathVariable("nombre") String nombre) {
+		return service.borrar(nombre, id);
 	}
 
 }
